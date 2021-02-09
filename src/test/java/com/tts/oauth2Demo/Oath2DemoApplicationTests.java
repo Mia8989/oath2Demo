9f65bbde46f1c8cff1644645251a7896a6d4e8fb
Mia8989 <74567329+Mia8989@users.noreply.github.com>
2021-02-09 11:09:15 -0600

    Initial commit

---
+++ src/test/java/com/tts/oauth2Demo/Oath2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
